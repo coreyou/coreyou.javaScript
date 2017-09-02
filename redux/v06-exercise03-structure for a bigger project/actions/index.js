f47b1aff264116a9a1a8a39dbb9166812c5d6537
coreyou <corey_ou@yahoo.com.tw>
2017-09-03 00:23:42 +0800

    [test] redux lesson

---
+++ redux/v06-exercise03-structure for a bigger project/actions/index.js
@@ -0,0 +1,13 @@
+// action creator
+
+var decrease = function(){
+    return { type: 'DECREMENT' };
+}
+
+var increase = function(){
+    return { type: 'INCREMENT' };
+}
+
+var getSum = function(a, b){
+    return { type: 'SUM', a:a, b:b };
+}
